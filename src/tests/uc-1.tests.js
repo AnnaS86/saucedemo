05c645db729bf283d6648d79a32e7e57ce219a24
Anna Sadovaia <lady.sadovaya@gmail.com>
2026-07-23 14:52:45 +0500

    test: add  username-only login test

--- src/tests/uc-1.tests.js
+++ src/tests/uc-1.tests.js
@@ -16,9 +16,12 @@ describe('UC-1 Form Validation (Negative Testing)', () => {
         await expect(errorUsername).toHaveText(expect.stringContaining('Username is required'));
     });
 
-    //  it('Attempt to login with Username only. Verify error: "Password is required"', async () => {
-
-    // });
+     it('Attempt to login with Username only. Verify error: "Password is required"', async () => {
+        await $('input[data-test="username"]').setValue('standard_user');
+        await $('input[data-test="login-button"]').click();
+        const errorPassword = await $('h3[data-test="error"]');
+        await expect(errorPassword).toHaveText(expect.stringContaining('Password is required'));
+    });
 
     //  it('Login with standard_user, go to Checkout, and attempt to continue without filling the postal code. Verify error message.', async () => {
 
