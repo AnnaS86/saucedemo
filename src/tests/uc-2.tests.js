6f3a63d19f6632aeca897e92128e1660c16acb9d
Anna Sadovaia <lady.sadovaya@gmail.com>
2026-08-04 16:30:34 +0500

    test: added explicit waitForDisplayed into uc-2

--- src/tests/uc-2.tests.js
+++ src/tests/uc-2.tests.js
@@ -10,10 +10,14 @@ describe('UC-2 Handling Latency (Wait Strategies)', () => {
         await $('input[data-test="username"]').setValue('performance_glitch_user');
         await $('input[data-test="password"]').setValue('secret_sauce');
         await $('input[data-test="login-button"]').click();
+        await $('button[id="react-burger-menu-btn"]').waitForDisplayed({ timeout: 6000 });
         //Burger Menu
         await $('button[id="react-burger-menu-btn"]').click();
+        await $('a[data-test="reset-sidebar-link"]').waitForDisplayed({ timeout: 6000 });
         //Reset
         await $('a[data-test="reset-sidebar-link"]').click();
+        const shoppingCartBadge = await $('span[data-test="shopping-cart-badge"]');
+        await expect(shoppingCartBadge).not.toBeDisplayed();
         //Logout
         await $('a[data-test="logout-sidebar-link"]').click();
         //Form is Visible
