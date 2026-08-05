8fc5265a844f9a0f43be783b52becc2dc6699b90
Anna Sadovaia <lady.sadovaya@gmail.com>
2026-08-05 20:07:45 +0500

    refactor: rewrite uc-2.tests.js using Page Objects Model

--- src/tests/uc-2.tests.js
+++ src/tests/uc-2.tests.js
@@ -1,29 +1,31 @@
 const LoginPage = require('./../po/pages/login.page');
+const InventoryPage = require('./../po/pages/inventory.page');
 
 const loginPage = new LoginPage();
+const inventoryPage = new InventoryPage();
 
 describe('UC-2 Handling Latency (Wait Strategies)', () => {
     beforeEach(async () => {
          await loginPage.open();
-         await $('input[data-test="username"]').setValue('');
-         await $('input[data-test="password"]').setValue('');
+         await loginPage.loginForm.usernameInput.setValue('');
+         await loginPage.loginForm.passwordInput.setValue('');
     });
 
     it('login with performance_glitch_user, reset app state, and logout successfully', async () => {
         //Autorization
-        await $('input[data-test="username"]').setValue('performance_glitch_user');
-        await $('input[data-test="password"]').setValue('secret_sauce');
-        await $('input[data-test="login-button"]').click();
-        await $('button[id="react-burger-menu-btn"]').waitForDisplayed({ timeout: 6000 });
+        await loginPage.loginForm.usernameInput.setValue('performance_glitch_user');
+        await loginPage.loginForm.passwordInput.setValue('secret_sauce');
+        await loginPage.loginForm.loginButton.click();
+        await inventoryPage.header.burgerButton.waitForDisplayed({ timeout: 6000 });
         //Burger Menu
-        await $('button[id="react-burger-menu-btn"]').click();
-        await $('a[data-test="reset-sidebar-link"]').waitForDisplayed({ timeout: 6000 });
+        await inventoryPage.header.burgerButton.click();
+        await inventoryPage.sideMenu.item('reset').waitForDisplayed({ timeout: 6000 });
         //Reset
-        await $('a[data-test="reset-sidebar-link"]').click();
-        const shoppingCartBadge = await $('span[data-test="shopping-cart-badge"]');
+        await inventoryPage.sideMenu.item('reset').click();
+        const shoppingCartBadge = await inventoryPage.header.cartBadge;
         await expect(shoppingCartBadge).not.toBeDisplayed();
         //Logout
-        await $('a[data-test="logout-sidebar-link"]').click();
+        await inventoryPage.sideMenu.item('logout').click();
         //Form is Visible
         const loginForm = await $('div[data-test="login-container"]');
         await expect(loginPage.loginForm.rootEl).toBeDisplayed();
